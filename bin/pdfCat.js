914b1ba2efabe405fcf575e6eda0cdbec0511088
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-08-24 13:01:07 +0200

    pdfCat

---
+++ bin/pdfCat.js
@@ -0,0 +1,8 @@
+var pdfSearch = require('./pdf');
+
+pdfSearch.open(process.argv[process.argv.length - 1]).then(function(pdf){
+	pdfSearch.readAllPages().then(function(pages){
+		console.log(pages.join('\n'));
+	});
+});
+
--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var pdfSearch = require('./pdf');
-
-pdfSearch.open('./pdf.pdf').then(function(pdf){
-	pdfSearch.readAllPages().then(function(pages){
-		console.log(pages.join('\n'));
-	});
-});
 
