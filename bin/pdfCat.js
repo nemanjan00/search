2e09d79570c86d5916041a75dd99d2bca3a9f1b7
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-08-24 13:07:13 +0200

    Package

--- bin/pdfCat.js
+++ bin/pdfCat.js
@@ -1,4 +1,5 @@
-var pdfSearch = require('../pdf');
+var path = require("path");
+var pdfSearch = require(path.join(__dirname, '../pdf'));
 
 pdfSearch.open(process.argv[process.argv.length - 1]).then(function(pdf){
 	pdfSearch.readAllPages().then(function(pages){
